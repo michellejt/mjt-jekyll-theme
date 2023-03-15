731fd3c331ca8f853647412a276d1ef4a181637b
michellejt <mtongas@gmail.com>
2023-03-15 17:07:00 +1030

    search files

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,61 +1,61 @@
-(function() {
-    function displaySearchResults(results, store) {
-      var searchResults = document.getElementById('search-results');
-  
-      if (results.length) { // Are there any results?
-        var appendString = '';
-  
-        for (var i = 0; i < results.length; i++) {  // Iterate over the results
-          var item = store[results[i].ref];
-          appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-          appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
-        }
-  
-        searchResults.innerHTML = appendString;
-      } else {
-        searchResults.innerHTML = '<li>No results found</li>';
-      }
+class jekyllSearch {
+  constructor(dataSource, searchField, resultsList, siteURL) {
+    this.dataSource = dataSource
+    this.searchField = document.querySelector(searchField)
+    this.resultsList = document.querySelector(resultsList)
+    this.siteURL = siteURL
+
+    this.displayResults = this.displayResults.bind(this)
+  }
+
+  fetchedData() {
+    return fetch(this.dataSource)
+      .then(blob => blob.json())
+  }
+
+  async findResults() {
+    const data = await this.fetchedData()
+    return data.filter(item => {
+      const regex = new RegExp(this.searchField.value, 'gi')
+      return item.title.match(regex) || item.content.match(regex)
+    })
+  }
+
+  async displayResults() {
+    const results = await this.findResults()
+    const html = results.map(item => {
+      return `
+        <li class="result">
+            <article class="result__article  article">
+                <h4>
+                  <a href="${this.siteURL + item.url}">${item.title}</a>
+                </h4>
+                <p>${item.excerpt}</p>
+            </article>
+        </li>`
+    }).join('')
+    if ((results.length == 0) || (this.searchField.value == '')) {
+      this.resultsList.innerHTML = `<p>Sorry, nothing was found</p>`
+    } else {
+      this.resultsList.innerHTML = html
     }
-  
-    function getQueryVariable(variable) {
-      var query = window.location.search.substring(1);
-      var vars = query.split('&');
-  
-      for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-  
-        if (pair[0] === variable) {
-          return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
-        }
-      }
+  }
+
+  init() {
+    const url = new URL(document.location)
+    if (url.searchParams.get("search")) {
+      this.searchField.value = url.searchParams.get("search")
+      this.displayResults()
     }
-  
-    var searchTerm = getQueryVariable('query');
-  
-    if (searchTerm) {
-      document.getElementById('search-box').setAttribute("value", searchTerm);
-  
-      // Initalize lunr with the fields it will be searching on. I've given title
-      // a boost of 10 to indicate matches on this field are more important.
-      var idx = lunr(function () {
-        this.field('id');
-        this.field('title', { boost: 10 });
-        this.field('author');
-        this.field('category');
-        this.field('content');
-      });
-  
-      for (var key in window.store) { // Add the data to lunr
-        idx.add({
-          'id': key,
-          'title': window.store[key].title,
-          'author': window.store[key].author,
-          'category': window.store[key].category,
-          'content': window.store[key].content
-        });
-  
-        var results = idx.search(searchTerm); // Get lunr to perform a search
-        displaySearchResults(results, window.store); // We'll write this in the next section
+    this.searchField.addEventListener('keyup', () => {
+      this.displayResults()
+      url.searchParams.set("search", this.searchField.value)
+      window.history.pushState('', '', url.href)
+    })
+    this.searchField.addEventListener('keypress', event => {
+      if (event.keyCode == 13) {
+        event.preventDefault()
       }
-    }
-  })();
+    })
+  }
+}
